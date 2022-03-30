58d718653dfdb80bbd1f3648650a759094cb433d
DESKTOP-08R6MMQ\user <hyein8290@gmail.com>
2022-03-30 09:31:49 +0900

    main.js 파일 분석

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue' //컴포넌트 파일
 
 Vue.config.productionTip = false
 
+var App = {
+  template: '<div>app</div>'
+}
+
 new Vue({
   render: h => h(App),
+  components: {
+    'app': App
+  }
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   // render: template이라는 속성을 정의했을때, 내부적으로 render가 실행됨
+//   render: h => h(App),
+// })
