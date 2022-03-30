b0b37c9f06feacb933ff16d6125f779a23319351
DESKTOP-08R6MMQ\user <hyein8290@gmail.com>
2022-03-30 12:53:29 +0900

    싱글 파일 컴포넌트 props, event emit

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,35 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- vscode 파일 바로가기 기능을 사용하기 위해서는 케밥 표기법을 따라야 한다. -->
-    <!-- <hello-world></hello-world> 
-    <HelloWorld></HelloWorld>
-    <HelloWorld/> -->
+  <!-- template root의 엘리먼트는 무조건 하나! -->
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+
+// import 할 때, 확장자가까지 써주는 걸 권장. (vscode 기능 활용)
+import AppHeader from './components/AppHeader.vue';
+
+// 예전 방식
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld,
-    // 'hello-world': HelloWorld,
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<!-- 
+    컴포넌트 파일 이름: 파스칼 케이스 + 두 단어 이상
+
+    만약 컴포넌트 이름이 한 단어라면,
+    브라우저가 html 표준 태그인지 vue의 컴포넌트 태그인지 구분할 수 없음
+-->
+<template>
+  <header>
+      <h1>{{ propsdata }}</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function() {
+            this.$emit('renew');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
