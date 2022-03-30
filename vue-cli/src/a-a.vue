061d608538801db22c5027d9e8d716169331ed71
DESKTOP-08R6MMQ\user <hyein8290@gmail.com>
2022-03-30 11:14:09 +0900

    싱글 파일 컴포넌트

---
+++ vue-cli/src/a-a.vue
@@ -0,0 +1,31 @@
+<!-- 
+    html, js, css를 한 파일에서 관리
+-->
+var appHeader = {
+    template: '<div>header</div>',
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
